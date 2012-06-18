1e6a3913aa56e1df3d6a16dc3ebe48a4fc61a901
David Klepacek <david.klepacek@seznam.cz>
2012-06-19 00:26:28 +0200

    fix? part 3

--- includes/functions/PlanetResourceUpdate.php
+++ includes/functions/PlanetResourceUpdate.php
@@ -9,5 +9,22 @@ function PlanetResourceUpdate ( $CurrentUser, &$CurrentPlanet, $UpdateTime, $Sim
         $shipyard = Legacies_Empire_Shipyard::factory($CurrentPlanet, $CurrentUser);
         $shipyard->updateQueue();
         $CurrentPlanet = $shipyard->save();
+            $sql =<<<SQL_EOF
+UPDATE {{table}} SET
+    `metal` = '{$CurrentPlanet['metal']}',
+    `crystal` = '{$CurrentPlanet['crystal']}',
+    `deuterium` = '{$CurrentPlanet['deuterium']}',
+    `last_update` = '{$CurrentPlanet['last_update']}',
+    `metal_perhour` = '{$CurrentPlanet['metal_perhour']}',
+    `crystal_perhour` = '{$CurrentPlanet['crystal_perhour']}',
+    `deuterium_perhour` = '{$CurrentPlanet['deuterium_perhour']}',
+    `energy_used` = '{$CurrentPlanet['energy_used']}',
+    `energy_max` = '{$CurrentPlanet['energy_max']}'
+  WHERE`id` = {$CurrentPlanet['id']}
+SQL_EOF;
+
+        doquery("LOCK TABLE {{table}} WRITE", 'planets');
+        doquery($sql, 'planets');
+        doquery("UNLOCK TABLES", '');
     }
 }
