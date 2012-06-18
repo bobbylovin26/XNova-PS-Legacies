438b76b0ae1eade6f2704fd8c79aaa3dd126d045
David Klepacek <david.klepacek@seznam.cz>
2012-06-19 00:42:37 +0200

    maybe?

--- includes/functions/PlanetResourceUpdate.php
+++ includes/functions/PlanetResourceUpdate.php
@@ -114,8 +114,28 @@ function PlanetResourceUpdate ( $CurrentUser, &$CurrentPlanet, $UpdateTime, $Sim
 			$CurrentPlanet['deuterium']  = $MaxDeuteriumStorage;
 		}
 	}
+  if ($Simul == false) {
+        $shipyard = Legacies_Empire_Shipyard::factory($CurrentPlanet, $CurrentUser);
+        $shipyard->updateQueue();
+        $CurrentPlanet = $shipyard->save();
+            $sql =<<<SQL_EOF
+UPDATE {{table}} SET
+    `metal` = '{$CurrentPlanet['metal']}',
+    `crystal` = '{$CurrentPlanet['crystal']}',
+    `deuterium` = '{$CurrentPlanet['deuterium']}',
+    `last_update` = '{$CurrentPlanet['last_update']}',
+    `metal_perhour` = '{$CurrentPlanet['metal_perhour']}',
+    `crystal_perhour` = '{$CurrentPlanet['crystal_perhour']}',
+    `deuterium_perhour` = '{$CurrentPlanet['deuterium_perhour']}',
+    `energy_used` = '{$CurrentPlanet['energy_used']}',
+    `energy_max` = '{$CurrentPlanet['energy_max']}'
+  WHERE`id` = {$CurrentPlanet['id']}
+SQL_EOF;
 
-
+        doquery("LOCK TABLE {{table}} WRITE", 'planets');
+        doquery($sql, 'planets');
+        doquery("UNLOCK TABLES", '');
+    }
 }
 
 // Revision History
